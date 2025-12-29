64d0e2c9d102cb3a4d7071d401661a86aee5a2e3
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-12-29 19:06:35 +0530

    Update auther.js

--- auther.js
+++ auther.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
  var allowedURLs = [
      "https://commingsoonmovies.online",
 "https://vacancynewstimes.in",
+     "https://xprimehub.click",
     "https://saanime.store",
      "https://www.vegamovues.cv",
      "https://www.vegamovues.top",
